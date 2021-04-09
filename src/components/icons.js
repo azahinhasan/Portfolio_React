032bff17106bac85e7c96bb2615dd7b36a5b1964
A H Zahin <azahinhasan@gmail.com>
2021-04-09 15:46:41 +0600

    css bug fixed

--- src/components/icons.js
+++ src/components/icons.js
@@ -17,8 +17,8 @@ class Icons extends Component {
     return (
     <div className={classes.icons}>
       <span className={classes.fastPageAnimation}>
-        <a href="mailto:azahinhasan@gmail.com?subject=Wanna Contact with You" target='_blank'><img src={Mail}/></a>
-        <a href='tel: +8801515667288' target='_blank'><img src={Phn}/></a>
+        <a  className={classes.iconAngorTag} href="mailto:azahinhasan@gmail.com?subject=Wanna Contact with You" target='_blank'><img src={Mail}/></a>
+        <a  className={classes.iconAngorTag}  href='tel: +8801515667288' target='_blank'><img src={Phn}/></a>
       </span>
         <br/>
         
@@ -26,7 +26,7 @@ class Icons extends Component {
         <a href='https://www.facebook.com/a.h.zahin/' target='_blank'><img src={FB}/></a>
         <a href='https://github.com/azahinhasan' target='_blank'><img src={GitHub}/></a>
         <a href='https://www.instagram.com/potato.coderr/' target='_blank'><img src={Insta}/></a>
-        <a  href='https://www.linkedin.com/in/ashhak-hasan-099455133/' target='_blank'><img src={LinkedIn}/></a>
+        <a href='https://www.linkedin.com/in/ashhak-hasan-099455133/' target='_blank'><img src={LinkedIn}/></a>
       </span>
 
     </div>
